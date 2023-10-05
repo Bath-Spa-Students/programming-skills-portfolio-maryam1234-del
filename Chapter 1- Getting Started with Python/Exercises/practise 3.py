823ca03ae4b25962dd5a15476437cbf1a674038f
maryam1234-del <maryam.ktmanzoor@gmail.com>
2023-10-05 13:35:47 +0400

    practise 3.py
    dots

---
+++ Chapter 1- Getting Started with Python/Exercises/practise 3.py
@@ -0,0 +1,3 @@
+# ques 3: you need to make a program for a leader board.the program need tol output the numbers 1 to 9,each on a separate line followed by a dot 
+for i in range (1,10):
+    print(i,'.')
