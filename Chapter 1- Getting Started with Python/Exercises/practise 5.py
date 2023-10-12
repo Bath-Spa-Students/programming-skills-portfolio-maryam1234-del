54668845d5da71f3de9f781f4446cdf262998a39
maryam1234-del <maryam.ktmanzoor@gmail.com>
2023-10-12 12:26:19 +0400

    practise 5.py

--- Chapter 1- Getting Started with Python/Exercises/practise 5.py
+++ Chapter 1- Getting Started with Python/Exercises/practise 5.py
@@ -1,1 +1,6 @@
 # ques 5: write four strings in different variables and print the output as one string. (string concatenation)
+firstname='maryam'
+lastname='manzoor' 
+university='bathspa'
+room='f05'
+print(firstname+lastname+university+room)
