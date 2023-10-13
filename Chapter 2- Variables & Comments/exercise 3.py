7c58110273f0dcd8f58d168fabfe44f442ce6dab
maryam1234-del <maryam.ktmanzoor@gmail.com>
2023-10-13 17:52:25 +0400

    exercise 3.py

---
+++ Chapter 2- Variables & Comments/exercise 3.py
@@ -0,0 +1,12 @@
+name="\tmaryam manzoor\n"
+print("pretty:")
+print(name)
+
+print("\nusing lstrip():")
+print (name.lstrip())
+
+print("\nusing rstrip():")
+print(name.rstrip())
+
+print("\nusing strip():")
+print(name.strip())
