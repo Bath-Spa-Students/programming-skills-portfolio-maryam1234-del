d18284adbb1abae39d4045b7b83274554e212dac
maryam1234-del <maryam.ktmanzoor@gmail.com>
2023-10-17 22:44:12 +0400

    3 exercise 3.py

---
+++ Chapter 3- Data Structures/3 exercise 3.py
@@ -0,0 +1,6 @@
+# Create a list of favorite modes of transportation
+vehicles = ["motorcycle", "car", "rickshaw", "airplane", "truck"]
+
+# Iterate through the list and print statements
+for vehicle in vehicles:
+    print(f"I would like to own a {vehicle.capitalize()}.")
