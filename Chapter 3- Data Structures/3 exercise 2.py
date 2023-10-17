6368ddcd508ac69fe716220e8c67da2319220afb
maryam1234-del <maryam.ktmanzoor@gmail.com>
2023-10-17 21:43:41 +0400

    3 exercise 2.py

---
+++ Chapter 3- Data Structures/3 exercise 2.py
@@ -0,0 +1,10 @@
+names = ['maryam', 'manzoor', 'khan']
+
+msg = "hi  wassup, " + names[0].title() + "!"
+print(msg)
+
+msg = "good afternooni, " + names[1].title() + "!"
+print(msg)
+
+msg = "gorgeous, " + names[2].title() + "!"
+print(msg)
